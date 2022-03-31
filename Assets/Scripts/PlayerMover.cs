bb5a050b148eadf3753def9b597090bea538bfd9
yomawari <anime112@bk.ru>
2022-03-31 18:32:09 +0500

    Появилось звуковое сопровождение шагов

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -10,10 +10,14 @@ public class PlayerMover : MonoBehaviour
     private Vector2 moveVelocity;
     private Animator anim;
 
+    public AudioClip[] footsteps;
+    [SerializeField] private AudioSource moveSound;
+
     private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        moveSound = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -22,21 +26,33 @@ public class PlayerMover : MonoBehaviour
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
             anim.Play("Tomoko_a_walk");
+           Footstep();
+
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
             anim.Play("Tomoko_d_walk");
+            Footstep();
         }
         else if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.up * speed * Time.deltaTime;
             anim.Play("Tomoko_w_walk");
+           Footstep();
         }
         else if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.down * speed * Time.deltaTime;
             anim.Play("Tomoko_s_walk");
+            Footstep();
         }
+    }
+
+    void Footstep()
+    {
+        int randInt = Random.Range(0, footsteps.Length);
+
+        moveSound.PlayOneShot(footsteps[randInt]);
     }
 }
